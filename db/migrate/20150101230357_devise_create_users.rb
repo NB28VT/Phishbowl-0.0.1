f23f5d08c3395a43f3157a9b0755b3bce9aa878f
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-01-01 18:56:37 -0500

    Tested user model with username

--- db/migrate/20150101230357_devise_create_users.rb
+++ db/migrate/20150101230357_devise_create_users.rb
@@ -5,8 +5,6 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
       t.string :username, null: false
 
-
-
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
