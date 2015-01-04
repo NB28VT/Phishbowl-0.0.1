cf427dd39f4902eab6b4570804507bdbdc37d496
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-01-04 15:28:39 -0500

    Refactored prediction unit tests with Factory Girl

--- spec/models/prediction_spec.rb
+++ spec/models/prediction_spec.rb
@@ -3,191 +3,45 @@ require 'rails_helper'
 RSpec.describe Prediction, :type => :model do
 
   it "allows a user to enter a valid predicition" do
-    prediction = Prediction.new
-
-    prediction.set_one_opener = "Farmhouse"
-    prediction.set_one_closer = "Possum"
-    prediction.set_two_opener = "Bathtub Gin"
-    prediction.set_two_closer = "Harry Hood"
-    prediction.encore = "Good Times Bad Times"
-    prediction.random_song_one = "Roggae"
-    prediction.random_song_two = "1999"
-    prediction.random_song_three = "Run Like an Antelope"
-    prediction.random_song_four = "Sing Monica"
-    prediction.random_song_five = "Farmhouse"
-    prediction.animal_song = "Possum"
-    prediction.gamehendge_song = "Icculus"
-    prediction.cover_song = "After Midnight"
-    prediction.place_song = "Farmhouse"
-    prediction.elemental_song = "Light"
-    prediction.name_song = "Sing Monica"
-
+    prediction = FactoryGirl.build(:prediction)
 
     expect(prediction.save).to eq(true)
   end
 
   it "does not allow a user to enter songs Phish hasn't played" do
-    prediction = Prediction.new
+    invalid_song_prediction = FactoryGirl.build(:prediction, set_one_opener: "Party in the USA")
 
-    prediction.set_one_opener = "Party in the USA"
-    prediction.set_one_closer = "Possum"
-    prediction.set_two_opener = "Bathtub Gin"
-    prediction.set_two_closer = "Run Like an Antelope"
-    prediction.encore = "Sing Monica"
-    prediction.random_song_one = "Farmhouse"
-    prediction.random_song_two = "Harpua"
-    prediction.random_song_three = "Mound"
-    prediction.random_song_four = "Roggae"
-    prediction.random_song_five = "Bathtub Gin"
-    prediction.animal_song = "Possum"
-    prediction.gamehendge_song = "Icculus"
-    prediction.cover_song = "Time Loves a Hero"
-    prediction.place_song = "Farmhouse"
-    prediction.elemental_song = "Light"
-    prediction.name_song = "Sing Monica"
-
-
-    expect(prediction.save).to eq(false)
+    expect(invalid_song_prediction.save).to eq(false)
   end
 
   it "does not allow a cover song to be a phish song" do
-    prediction = Prediction.new
+    invalid_cover_prediction = FactoryGirl.build(:prediction, cover_song: "Possum")
 
-    prediction.set_one_opener = "Farmhouse"
-    prediction.set_one_closer = "Possum"
-    prediction.set_two_opener = "Bathtub Gin"
-    prediction.set_two_closer = "Harry Hood"
-    prediction.encore = "Good Times Bad Times"
-    prediction.random_song_one = "Roggae"
-    prediction.random_song_two = "1999"
-    prediction.random_song_three = "Run Like an Antelope"
-    prediction.random_song_four = "Sing Monica"
-    prediction.random_song_five = "Farmhouse"
-    prediction.animal_song = "Possum"
-    prediction.gamehendge_song = "Icculus"
-    prediction.cover_song = "Possum"
-    prediction.place_song = "Farmhouse"
-    prediction.elemental_song = "Light"
-    prediction.name_song = "Sing Monica"
-
-    expect(prediction.save).to eq(false)
+    expect(invalid_cover_prediction.save).to eq(false)
   end
 
   it "does not validate a non-animal song" do
-    prediction = Prediction.new
+    invalid_animal_prediction = FactoryGirl.build(:prediction, animal_song: "Light")
 
-    prediction.set_one_opener = "Farmhouse"
-    prediction.set_one_closer = "Possum"
-    prediction.set_two_opener = "Bathtub Gin"
-    prediction.set_two_closer = "Harry Hood"
-    prediction.encore = "Good Times Bad Times"
-    prediction.random_song_one = "Roggae"
-    prediction.random_song_two = "1999"
-    prediction.random_song_three = "Run Like an Antelope"
-    prediction.random_song_four = "Sing Monica"
-    prediction.random_song_five = "Farmhouse"
-    prediction.animal_song = "Light"
-    prediction.gamehendge_song = "Icculus"
-    prediction.cover_song = "1999"
-    prediction.place_song = "Farmhouse"
-    prediction.elemental_song = "Light"
-    prediction.name_song = "Sing Monica"
-
-    expect(prediction.save).to eq(false)
+    expect(invalid_animal_prediction.save).to eq(false)
   end
 
   it "does not validate a non-Gamehendge song" do
-    prediction = Prediction.new
+    invalid_gamehendge_prediction = FactoryGirl.build(:prediction, gamehendge_song: "Farmhouse")
 
-    prediction.set_one_opener = "Farmhouse"
-    prediction.set_one_closer = "Possum"
-    prediction.set_two_opener = "Bathtub Gin"
-    prediction.set_two_closer = "Harry Hood"
-    prediction.encore = "Good Times Bad Times"
-    prediction.random_song_one = "Roggae"
-    prediction.random_song_two = "1999"
-    prediction.random_song_three = "Run Like an Antelope"
-    prediction.random_song_four = "Sing Monica"
-    prediction.random_song_five = "Farmhouse"
-    prediction.animal_song = "Possum"
-    prediction.gamehendge_song = "Farmhouse"
-    prediction.cover_song = "1999"
-    prediction.place_song = "Farmhouse"
-    prediction.elemental_song = "Light"
-    prediction.name_song = "Sing Monica"
-
-    expect(prediction.save).to eq(false)
+    expect(invalid_gamehendge_prediction.save).to eq(false)
   end
 
-  it "does not validate a non-Gamehendge song" do
-    prediction = Prediction.new
-
-    prediction.set_one_opener = "Farmhouse"
-    prediction.set_one_closer = "Possum"
-    prediction.set_two_opener = "Bathtub Gin"
-    prediction.set_two_closer = "Harry Hood"
-    prediction.encore = "Good Times Bad Times"
-    prediction.random_song_one = "Roggae"
-    prediction.random_song_two = "1999"
-    prediction.random_song_three = "Run Like an Antelope"
-    prediction.random_song_four = "Sing Monica"
-    prediction.random_song_five = "Farmhouse"
-    prediction.animal_song = "Possum"
-    prediction.gamehendge_song = "Farmhouse"
-    prediction.cover_song = "1999"
-    prediction.place_song = "Possum"
-    prediction.elemental_song = "Light"
-    prediction.name_song = "Sing Monica"
-
-    expect(prediction.save).to eq(false)
-  end
 
   it "does not validate a non-Elemental song" do
-    prediction = Prediction.new
+    invalid_elemental_prediction = FactoryGirl.build(:prediction, elemental_song: "Farmhouse")
 
-    prediction.set_one_opener = "Farmhouse"
-    prediction.set_one_closer = "Possum"
-    prediction.set_two_opener = "Bathtub Gin"
-    prediction.set_two_closer = "Harry Hood"
-    prediction.encore = "Good Times Bad Times"
-    prediction.random_song_one = "Roggae"
-    prediction.random_song_two = "1999"
-    prediction.random_song_three = "Run Like an Antelope"
-    prediction.random_song_four = "Sing Monica"
-    prediction.random_song_five = "Farmhouse"
-    prediction.animal_song = "Possum"
-    prediction.gamehendge_song = "Farmhouse"
-    prediction.cover_song = "1999"
-    prediction.place_song = "Possum"
-    prediction.elemental_song = "Farmhouse"
-    prediction.name_song = "Sing Monica"
-
-    expect(prediction.save).to eq(false)
+    expect(invalid_elemental_prediction.save).to eq(false)
   end
 
   it "does not validate a non-name song" do
-    prediction = Prediction.new
+    invalid_name_prediction = FactoryGirl.build(:prediction, name_song: "Farmhouse")
 
-    prediction.set_one_opener = "Farmhouse"
-    prediction.set_one_closer = "Possum"
-    prediction.set_two_opener = "Bathtub Gin"
-    prediction.set_two_closer = "Harry Hood"
-    prediction.encore = "Good Times Bad Times"
-    prediction.random_song_one = "Roggae"
-    prediction.random_song_two = "1999"
-    prediction.random_song_three = "Run Like an Antelope"
-    prediction.random_song_four = "Sing Monica"
-    prediction.random_song_five = "Farmhouse"
-    prediction.animal_song = "Possum"
-    prediction.gamehendge_song = "Farmhouse"
-    prediction.cover_song = "1999"
-    prediction.place_song = "Possum"
-    prediction.elemental_song = "Farmhouse"
-    prediction.name_song = "Farmhouse"
-
-    expect(prediction.save).to eq(false)
+    expect(invalid_name_prediction.save).to eq(false)
   end
-
-
-
 end
--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -8,4 +8,26 @@ FactoryGirl.define do
     password_confirmation 'password'
   end
 
+
+  factory :prediction do
+    set_one_opener "Farmhouse"
+    set_one_closer "Possum"
+    set_two_opener "Bathtub Gin"
+    set_two_closer "Harry Hood"
+    encore "Good Times Bad Times"
+    random_song_one "Roggae"
+    random_song_two "1999"
+    random_song_three "Run Like an Antelope"
+    random_song_four "Sing Monica"
+    random_song_five "Farmhouse"
+    animal_song "Possum"
+    gamehendge_song "Icculus"
+    cover_song "After Midnight"
+    place_song "Farmhouse"
+    elemental_song "Light"
+    name_song "Sing Monica"
+  end
+
+
+
 end
